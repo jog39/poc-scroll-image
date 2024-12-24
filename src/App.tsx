0c226ca98d67de8275ff1f4258e130e34908de41
adm2_dcoudrin_ext <dcoudrin_ext@christiandior.com>
2024-12-24 12:49:03 +0100

    Fix index

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { ImageViewer } from './components/ImageViewer';
 
-const buildImageUrl = index => {
+const buildImageUrl = (index: number) => {
   if (index < 10) {
-    return `/images/out-00{index + 1}.jpg`
+    return `/images/out-00${index + 1}.jpg`
   }
   if (index >= 10 && index < 100) {
-    return `/images/out-0{index + 1}.jpg`
+    return `/images/out-0${index + 1}.jpg`
   }
-  return `/images/out-{index + 1}.jpg`
+  return `/images/out-${index + 1}.jpg`
 }
 
 const images = Array.from(Array(239).keys()).map(buildImageUrl)
